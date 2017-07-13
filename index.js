92c8ad85b977142a3a44bdea40d82f3a344627bd
panda <xivistudios@gmail.com>
2017-07-13 11:33:20 +0800

    remove

--- index.js
+++ index.js
@@ -1,87 +1,41 @@
+'use strict'
+
+const Module = require('./module/module.js')
+const configuration = require('./configuration.js')
+const router = require('./router/root.js')
 const express = require('express')
-const MongoDB = require('mongodb')
-const cluster = require('cluster')
 const bodyParser = require('body-parser')
-const Websocket = require('socket.io')
-const os = require('os')
-const fs = require('fs')
+const Websocket = require('socket.io').listen(configuration.websocket.port)
 const app = express()
 
 
-const map = [1,2,3,4,5,6,7,8,9,0,'a','A','b','B','c','C','d','D','e','E','f','F','g','G','h','H','i','I','j','J','k','l','L','m','M','n','N','o','O','p','P','q','Q','r','R','s','S','t','T','u','U','v','V','w','W','x','X','y','Y','z','Z']
+// 全局变量
+global.INDEXPATH = __dirname
+global.CONFIGURATION = configuration
 
 
-exports.listen = function(port){
-    app.listen(port)
-    app.use(bodyParser.json())
-    app.use(bodyParser.urlencoded({ extended: true}))
-    app.use(express.static(`${__dirname}/module`))
-}
+// 初始化appcache
+// 连接数据库
+Module.appcache()
+Module.mongodb()
 
 
-exports.page = function(page){
-    let name = Object.keys(page)
-    let sum = 0
-    let obj = new Object
-    for(let i of name){
-        try{
-            fs.readFile(page[i], function(Error, data){
-                obj[i] = new Function('data', 'return `' + data.toString() + '`')
-                if(sum == name.length-1){
-                    global.Module = obj
-                }
-                sum += 1
-            })
-        }catch(err){
-            console.log(err)
-        }
-    }
-}
+// 解析中间件绑定
+// 文件资源
+// 静态资源
+// 版本控制
+// 路由中间件
+// 绑定端口
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(configuration.storage.path, express.static(`${__dirname}${configuration.storage.path}`))
+app.use('/public', express.static(`${__dirname}/page/public`))
+app.use('/appcache', express.static(`${__dirname}/page/appcache`))
+app.use('/', router)
+app.listen(configuration.express.port)
 
 
-app.get('/test', function(req, res){
-    res.cookie('SocketId', (
-            map[Math.round(Math.random()*60)] + 
-            map[Math.round(Math.random()*60)] + 
-            map[Math.round(Math.random()*60)] + 
-            map[Math.round(Math.random()*60)] + 
-            map[Math.round(Math.random()*60)] + 
-            map[Math.round(Math.random()*60)] + 
-            map[Math.round(Math.random()*60)] + 
-            map[Math.round(Math.random()*60)] + 
-            map[Math.round(Math.random()*60)] + 
-            map[Math.round(Math.random()*60)] + 
-            map[Math.round(Math.random()*60)] + 
-            map[Math.round(Math.random()*60)] + '-' + 
-            Math.round(Math.random()*10) + 
-            Math.round(Math.random()*10) + 
-            Math.round(Math.random()*10) + 
-            Math.round(Math.random()*10)
-        ), {path: '/'})
-    res.sendFile( __dirname + '/page/view/index.html')
+// WebSocket连接
+Websocket.sockets.on('connection', (socket) => {
+    console.log('socket', socket.id)
 })
-
-
-exports.view = function(name, value, Method){
-    app[Method != undefined ? Method : 'get'](name, function(req, res){
-        res.cookie('SocketId', (
-            map[Math.round(Math.random()*60)] + 
-            map[Math.round(Math.random()*60)] + 
-            map[Math.round(Math.random()*60)] + 
-            map[Math.round(Math.random()*60)] + 
-            map[Math.round(Math.random()*60)] + 
-            map[Math.round(Math.random()*60)] + 
-            map[Math.round(Math.random()*60)] + 
-            map[Math.round(Math.random()*60)] + 
-            map[Math.round(Math.random()*60)] + 
-            map[Math.round(Math.random()*60)] + 
-            map[Math.round(Math.random()*60)] + 
-            map[Math.round(Math.random()*60)] + '-' + 
-            Math.round(Math.random()*10) + 
-            Math.round(Math.random()*10) + 
-            Math.round(Math.random()*10) + 
-            Math.round(Math.random()*10)
-        ), {path: '/'})
-        res.send(Module[name] != undefined ? Module[name](value) : value)
-    })
-}
