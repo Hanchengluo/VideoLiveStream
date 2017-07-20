623f727a79966df0f295a0a911c1313fec94a127
panda <xivistudios@gmail.com>
2017-07-21 00:46:53 +0800

    Update index.js

--- page/public/js/index.js
+++ page/public/js/index.js
@@ -2,7 +2,7 @@ window.onload = () => {
     
     
     // 初始化Vue
-    Module = new Vue(new function(){
+    let Module = new Vue(new function(){
         // 组件节点
         this.el = '#docker'
         // 组件参数
@@ -14,6 +14,8 @@ window.onload = () => {
             MenuTips:false,
             // 通知
             Notification:false,
+            // 用户信息
+            MenuUser: false,
             // 用户
             User:{
                 'opacity':0,
@@ -21,10 +23,11 @@ window.onload = () => {
             },
             // 登录
             Login:{
-                'opacity':1
+                'opacity': 1,
+                'z-index': 1
             },
             // 通知
-            NotificationTab:{
+            NotificationStyle:{
                 'opacity':0,
                 'z-index': -100
             },
@@ -44,9 +47,35 @@ window.onload = () => {
                 Module.Notification = (Module.Notification == false) ? true : false
                 Module.MenuTips = false
                 Module.MenuUser = false
-            }
+            },
+            // 用户信息
+            HeadUserTab: () => {
+                Module.MenuUser = (Module.MenuUser == false) ? true : false
+                Module.MenuTips = false
+                Module.Notification = false
+            },
         }
     })
+    
+    
+    // 判断是否已经登录
+    if(localStorage.UserName != undefined && localStorage.PassWord != undefined){
+        $.post('./Login', {
+            name: localStorage.UserName,
+            pass: localStorage.PassWord,
+            decrypt: true
+        }, (data) => {
+            if(data.code == 200){
+                Module.NotificationStyle.opacity = 1
+                Module.NotificationStyle["z-index"] = 1
+                Module.User.opacity = 1
+                Module.User["z-index"] = 1
+                Module.Login.opacity = 0
+                Module.Login["z-index"] = '-100'
+                Module.search.right = '180px'
+            }
+        })
+    }
     
     
 }
